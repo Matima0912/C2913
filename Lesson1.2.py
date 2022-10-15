52bae00598649a911ce50b887d9e0585312762e9
Matima0912 <115864334+Matima0912@users.noreply.github.com>
2022-10-15 10:04:47 +0300

    Add files via upload

---
+++ Lesson1.2.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import *
+
+def test(argument):
+    print(argument)
+
+test("test")
+
+list = []
+list.append("hello")
+print(list)
+mnoj = {}
+print(f"My list {list}")
